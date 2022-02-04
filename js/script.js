3ea181a8e932217d8b04766c5329a986bfc26954
DatNeme <neme-b@live.com.ar>
2022-02-04 19:07:56 -0300

    Primer commit
    El simulador es funcional pero le faltan algunos detalles

---
+++ js/script.js
@@ -0,0 +1,64 @@
+class Auto {
+  constructor (marca, modelo, year, color){
+    this.marca = marca;
+    this.modelo = modelo;
+    this.year = year;
+    this.color = color;
+  }
+}
+
+class Pantalla {
+  nuevoItem(nuevoAuto){
+   const listaPartes = document.getElementById('lista');
+   const elemento = document.createElement('div');
+   elemento.innerHTML = `
+   <div class="card text-center mb-4">
+      <div class="card-body">
+        <strong>Marca</strong>: ${nuevoAuto.marca}
+        <strong>Modelo</strong>: ${nuevoAuto.modelo}
+        <strong>Año</strong>: ${nuevoAuto.year}
+        <strong>Color</strong>: ${nuevoAuto.color}
+        <a href="#" class="btn btn-danger" name="borrar">Quitar</a>
+      </div>
+   </div>
+   `;
+   listaPartes.appendChild(elemento);
+  }
+
+  limpiarForm(){
+    document.getElementById('formulario').reset();
+  }
+
+
+
+  eliminarItem(eliminar){
+    if (eliminar.name === 'borrar') {
+      eliminar.parentElement.parentElement.remove();
+    }
+  }
+
+  alertItem(){
+
+  }
+}
+
+document.getElementById('formulario')
+  .addEventListener('submit', function (e) {
+    const marca = document.getElementById('marca').value;
+    const modelo = document.getElementById('modelo').value;
+    const year = document.getElementById('year').value;
+    const color = document.getElementById('color').value;
+
+    const nuevoAuto = new Auto (marca, modelo, year, color);
+
+    const nuevosDatos = new Pantalla ();
+    nuevosDatos.nuevoItem(nuevoAuto);
+    nuevosDatos.limpiarForm();
+
+    e.preventDefault();
+})
+
+document.getElementById('lista').addEventListener('click', function (e) { 
+  const eliminar = new Pantalla();
+  eliminar.eliminarItem(e.target);
+ });
